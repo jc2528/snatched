c3d1a43faa42d73b24125bf2f07b8f7fd4f6c145
jc2528 <32905079+jc2528@users.noreply.github.com>
2018-07-20 00:29:53 -0400

    Add files via upload

--- snatched_cnn.py
+++ snatched_cnn.py
@@ -5,9 +5,16 @@ import pandas as pd
 
 data1 = pd.read_excel('accel_x_final_dataset.xlsx')
 
+dataset = data1.values
+
+data = dataset[:,0:13].astype(float) # sensor data
+
+
+#Y = dataset[:,12].astype(int)
+
 print(data1.shape)
 
-data = np.array(data1)[1:, [0,12]]
+#data = np.array(data1)[1:, [0,12]]
 #print(data[:,12])
 #data = data.tranpose()
 print(data.shape)
@@ -16,26 +23,45 @@ m = len(data)
 train = round(m/2)
 valid = train + round(m/4)
 
+test_x = data[valid:m, :-1]
+test_y = data[valid:m, -1]
+
 def make_nn(data):
 
-    train_x = data[0:train, 0]
+    train_x = data[0:train, :-1]
     train_y = data[0:train, -1]
 
-    val_x = data[train:valid, 0]
+    val_x = data[train:valid, :-1]
     val_y = data[train:valid, -1]
 
-    test_x = data[valid:m, 0]
-    test_y = data[valid:m, -1]
+
 
     model = Sequential()
-    model.add(Dense(1, input_dim= 1, activation = 'relu'))
+    model.add(Dense(15, input_dim= 12, activation='relu'))
+    #model.add(Dense(15, activation='relu'))
+    #model.add(Dense(15, activation='relu'))
+    model.add(Dense(1, activation='softmax'))
+    #model.add(Dense(1, input_dim = 2, activation = 'relu'))
     model.compile(optimizer= 'SGD', loss='binary_crossentropy', metrics=['accuracy'])
     model.fit(train_x, train_y, epochs = 15, batch_size = 10, validation_data=[val_x, val_y])
 
     scores = model.evaluate(train_x, train_y)
     #print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
-    return model.outputs
+    return model
 
-    ynew = model.predict_classes(test_x)
+model = make_nn(data)
+print(model.outputs)
 
-print(make_nn(data))
+#test_x = data[valid:m, 0]
+#test_y = data[valid:m, -1]
+ynew = model.predict_classes(test_x)
+#print(sum(test_y))
+#print(sum(ynew))
+
+
+
+
+
+
+
+
